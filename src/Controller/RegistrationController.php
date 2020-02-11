d497dd5d1d412f371c8af1f8dc571476be7b40a2
sylvain <priou.sylvain@gmail.com>
2020-02-11 10:27:28 +0100

    update user roles

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -11,6 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
+
 class RegistrationController extends Controller
 {
     /**
