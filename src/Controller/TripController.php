c7969d5f534eafc14d01a6ad81aae0fea7dfee1f
caro <caroleanne.basson2019@campus-eni.fr>
2020-02-11 09:21:32 +0100

    Modification of Tripcontroller/TripType/TwigCreate

--- src/Controller/TripController.php
+++ src/Controller/TripController.php
@@ -2,18 +2,48 @@
 
 namespace App\Controller;
 
+use App\Entity\Trip;
+use App\Form\TripType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/trip", name="trip_")
+ */
 
 class TripController extends AbstractController
 {
     /**
-     * @Route("/trip", name="trip")
+     * @Route("/trip2", name="trip2_")
      */
     public function index()
     {
-        return $this->render('trip/index.html.twig', [
-            'controller_name' => 'TripController',
-        ]);
+        return $this->render(
+            'trip/index.html.twig',
+            [
+                'controller_name' => 'TripController',
+            ]
+        );
+    }
+
+
+    /**
+     * @Route("/createtrip", name="createtrip_")
+     */
+    public function createtrip(Request $request, EntityManagerInterface $entityManager)
+    {
+
+        $trip = $this->getUser();
+        $tripForm = $this->createForm(TripType::class, $trip);
+
+        return $this->render(
+            'trip/create.html.twig',
+            [
+                'tripFormView' => $tripForm->createView(),
+            ]
+        );
+
     }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -18,7 +18,6 @@ class User implements UserInterface
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
-     * @UniqueEntity(fields={"email"}, message="There is already an account with this pseudo")
      */
     private $id;
 
