6bf2999e8b098acbb2ef117db2aff12bdd8cff14
emilie <emilie.cornic2019@campus-eni.fr>
2020-02-13 10:09:04 +0100

    changement mot de passe

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -26,77 +26,17 @@ class MainController extends AbstractController
         $tripRepository = $entityManager->getRepository(Trip::class);
         $allTrips = $tripRepository->findAll();
 
-
-
         $userTripRepository = $entityManager->getRepository(Trip::class);
-        $allUsersTrips = $userTripRepository->findUsersByTrips();
+        $allTrips = $userTripRepository->findAll();
 
-//        dump($allUsersTrips);
-//        die();
-
-        return $this->render('trip/index.html.twig', compact('allTrips','allUsersTrips'));
-
+        return $this->render('trip/index.html.twig', compact('allTrips'));
 
     }
 
 
-    /**
-     * @Route("/login", name="login")
-     */
-    public function login(Request $request, AuthenticationUtils $authenticationUtils)
-    {
-        // get the login error if there is one
-        $error = $authenticationUtils->getLastAuthenticationError(); // Est-ce qu'il y a eu des erreurs ?
-
-        // last name entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
-
-        return $this->render(
-            'main/login.html.twig',
-            [
-                'last_username' => $lastUsername, // dernier username connecté
-                'error' => $error, // les erreurs de connexion
-            ]
-        );
-
-    }
 
 
-    /**
-     * @route("profil", name="profil")
-     */
-    public function profil (Request $request, EntityManagerInterface $entityManager)
-    {
-      // récupérer le user qui est connecté
 
-        $user = $this->getUser();
-
-
-        // créer un formulaire Usertype et y ajouter le User
-        $userProfil = $this->createForm(UserUpdateType::class,$user);
-        $userProfil->handleRequest($request);
-
-        // traitement après soumission du form
-        if ($userProfil->isSubmitted() && $userProfil->isValid()) {
-
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($user);
-            $entityManager->flush();
-
-            $this->addFlash('success', 'profil modifié !');
-
-            return $this->redirectToRoute('home');
-        }
-
-
-        return $this->render('main/profil.html.twig', ['userFormView' => $userProfil->createView()]);
-
-
-// envoyer le formulaire dans le template
-// si le formulaire est soumis il persister et flusher
-
-
-    }
 
 
 
---
+++ src/Form/ChangePasswordType.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Security\Core\Validator\Constraints\UserPassword;
+
+class ChangePasswordType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+
+
+            ->add('oldPassword', PasswordType::class, [
+               'required'=> true,
+                ])
+
+
+            ->add('newPassword', RepeatedType::class, array(
+                'type' => PasswordType::class,
+                'mapped' => false,
+                'invalid_message' => 'Les deux mots de passe doivent être identiques',
+                'first_options'=> array ('label' => 'Nouveau mot de passe'),
+                'second_options'=>array ('label' => 'Confirmer votre nouveau mot de passe'),
+                'required' => true,
+            ))
+
+
+            ->add('submit', SubmitType::class, array(
+                'attr' => array(
+                    'class' => 'btn btn-primary btn-block'
+                )
+            ));
+
+
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
