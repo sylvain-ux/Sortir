d268a508f7e0382be865de423a7e7b5f92cac1c3
sylvain <priou.sylvain@gmail.com>
2020-02-19 17:31:50 +0100

    fix conflict

--- config/bundles.php
+++ config/bundles.php
@@ -10,9 +10,5 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
-<<<<<<< HEAD
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
-=======
-    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
->>>>>>> 41b339328a0a3ebcfa014ed8a1897f4714b79670
 ];
